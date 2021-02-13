a4d55360286d241d0e6e1814ce4514a3edc5c4a5
Tim Franklin <tim@pelican.org>
2021-02-13 16:10:23 +0000

    Validate Game destroys dependant Races.

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -7,5 +7,17 @@ RSpec.describe Game, type: :model do
     it "requires new game to be in waiting state" do
       expect(subject.waiting?).to be
     end
+    it "requires deleted game to delete all attached races" do
+      subject.name = "Alpha"
+      subject.save!
+      race = subject.races.create(name: "Bob")
+      race.save!
+      subject.destroy!
+      # Should be no Races left in the DB at this point
+      # TODO: this is likely to fail as soon as we have fixtures
+      # TODO: find a better way to test
+      race = Race.take
+      expect(race).to be_nil
+    end
   end
 end
